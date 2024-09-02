4ec6caa56d3a6d05086b49b27386257fdbd69fef
george.karageorgiou@gmail.com <george.karageorgiou@gmail.com>
2024-09-02 08:12:13 +0000

    removing trading account pnl left join from ta with brand

--- models/uticen_ta_with_brand.sql
+++ models/uticen_ta_with_brand.sql
@@ -3,9 +3,9 @@ SELECT
   ta.client,
   twb.brand_name,
   concat_ws('_', twb.client_id, twb.brand_name) as client_id_brand,
-  pnl.balance,
-  abs(pnl.balance) as abs_balance
+  NULL AS balance,
+  NULL as abs_balance
 FROM sales_uticen.trading_accounts ta
-  LEFT JOIN imports.uticen_trading_accounts_pnl pnl ON pnl.login = ta.login
+  --LEFT JOIN imports.uticen_trading_accounts_pnl pnl ON pnl.login = ta.login
   JOIN sales_uticen.uticen_transactions_with_brand twb ON ta.client = twb.client_id
-ORDER BY abs(pnl.balance) DESC
+--ORDER BY abs(pnl.balance) DESC
